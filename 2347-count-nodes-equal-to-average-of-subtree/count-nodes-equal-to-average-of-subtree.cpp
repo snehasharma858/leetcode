03c6c3b1fe88a1358bf8bf9572f204f630af0aa0
snehasharma858 <241312022@nitdelhi.ac.in>
2026-09-11 00:10:33 +0530

    Time: 3 ms (91.06%) | Memory: 15.8 MB (64.72%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+
+    pair<int, int> dfs(TreeNode* root) {
+        if (!root)
+            return {0, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int sum = root->val + left.first + right.first;
+        int count = 1 + left.second + right.second;
+
+        if (sum / count == root->val)
+            ans++;
+
+        return {sum, count};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
